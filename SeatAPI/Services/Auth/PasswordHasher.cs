fe75b80c747c8c2fe4c7bdf817fae6046a7a353b
AminAbdo <AminAbdo69>
2025-02-18 23:41:26 +0200

    add: auth services

---
+++ SeatAPI/Services/Auth/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SeatAPI.Services.Auth
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            byte[] computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}
